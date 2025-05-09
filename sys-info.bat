ea1d99df07b089675c504374578ae59fec9f7ace
IronCubeStudios <orangutanggreen722@gmail.com>
2025-05-09 12:55:30 -0400

    created sys info script

---
+++ sys-info.bat
@@ -0,0 +1,26 @@
+@echo off
+
+
+set /p "save_or_not=Do you want to save the report or print it? [y/n] "
+
+if /i "%save_or_not%" == "y" (
+	echo ==== SYSTEM INFO ==== > system_report.txt
+	systeminfo >> system_report.txt
+	echo ==== CPU INFO ==== >> system_report.txt
+	wmic cpu get name, maxclockspeed, currentclockspeed >> system_report.txt
+	echo ==== RAM ==== >> system_report.txt
+	wmic memorychip get capacity, speed, manufacturer >> system_report.txt
+	echo ==== GPU ==== >> system_report.txt
+	wmic path win32_VideoController get name >> system_report.txt
+	echo ==== DISK ==== >> system_report.txt
+	wmic diskdrive get model, size >> system_report.txt
+	start system_report.txt
+)
+
+if /i "%save_or_not%" == "n" (
+	systeminfo
+	wmic cpu get name, maxclockspeed, currentclockspeed
+	wmic memorychip get capacity, speed, manufacturer
+	wmic path win32_VideoController get name
+	wmic diskdrive get model, size
+)
